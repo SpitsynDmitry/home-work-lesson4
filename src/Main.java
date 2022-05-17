0a4b292f7329da68a3787ca9ba441125be0085b3
Dmitry <d.spitsyn@internet.ru>
2022-05-17 10:30:11 +0300

    выполнил дз

--- src/Main.java
+++ src/Main.java
@@ -7,8 +7,17 @@ public class Main {
         long elephant =110L;
         double sugarWeight =4.5;
         float saltWeight = 1.5f;
-        boolean dogIsAdult = dog > 10;
+        boolean dogIsAdult = dog < 10;
         char bar = 36;
         System.out.println (bar);
+
+        // Задание 2
+        double boxer1 = 78.2;
+        double boxer2 = 82.7;
+        double weightOfAllBoxers = boxer1 + boxer2;
+        double weightDifference = boxer2 - boxer1;
+        System.out.println ("Общий вес боксеров " + weightOfAllBoxers);
+        System.out.println ("Разница в весе боксеров " + weightDifference);
+
     }
 }
