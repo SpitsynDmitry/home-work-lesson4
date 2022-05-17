005cb873726b46b27f78d1a002c23638a5b4cbe9
Dmitry <d.spitsyn@internet.ru>
2022-05-17 10:09:58 +0300

    выполнил дз

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Пишем наше дз!!");
-        System.out.println("Делаем коммит и пуш");
-        System.out.println("Переходим на страницу в гидхаб");
+        // 3адание 1
+        byte cat = 1;
+        short mouse = 10;
+        int dog = 8;
+        long elephant =110L;
+        double sugarWeight =4.5;
+        float saltWeight = 1.5f;
+        boolean dogIsAdult = dog > 10;
+        char bar = 36;
+        System.out.println (bar);
     }
 }
